6386f8059232e5175b1eb718f88f9ea61de3f581
minhhao0 <153645230+minhhao0@users.noreply.github.com>
2026-04-27 23:23:25 +0700

    Update solution for lstm-cell-state

--- lstm/lstm-cell-state/lstm-cell-state.py
+++ lstm/lstm-cell-state/lstm-cell-state.py
@@ -4,5 +4,5 @@ def update_cell_state(C_prev: np.ndarray, f_t: np.ndarray,
                       i_t: np.ndarray, c_tilde: np.ndarray) -> np.ndarray:
     """Update cell state: C_t = f_t * C_prev + i_t * c_tilde"""
     # YOUR CODE HERE
-    C_t = f_t*C_prev+i_t*c_tilde
+    C_t=f_t*C_prev+i_t*c_tilde
     return C_t
