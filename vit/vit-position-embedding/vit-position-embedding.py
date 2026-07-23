a199c26b2088f706ae1f3aac0a306f4982b3869e
minhhao0 <153645230+minhhao0@users.noreply.github.com>
2026-07-23 17:15:51 +0700

    Solve vit-position-embedding

--- vit/vit-position-embedding/vit-position-embedding.py
+++ vit/vit-position-embedding/vit-position-embedding.py
@@ -6,6 +6,8 @@ def add_position_embedding(patches: np.ndarray, num_patches: int, embed_dim: int
     pos_embed: position embedding of shape (1, N, D). If None, initialize randomly.
     """
     # YOUR CODE HERE
+    if pos_embed is None:
+        pos_embed=np.random.randn(1,num_patches,embed_dim)*0.02
     patches=np.array(patches)
     pos_embed=np.array(pos_embed)
     pos_embed=pos_embed[0]
